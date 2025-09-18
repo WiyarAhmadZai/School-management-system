576027f8f06c5c0b242f71bbe4c3d087951ab7d1
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-18 16:32:33 +0430

    created

---
+++ school-management/app/Models/Grade.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'grade',
+        'marks',
+        'remarks',
+        'date',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'marks' => 'decimal:2',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
