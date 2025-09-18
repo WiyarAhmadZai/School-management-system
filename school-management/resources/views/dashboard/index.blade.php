92636e05f9c560f973d54f36658e2ec8b7ce4c73
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-18 15:54:34 +0430

    updated

--- school-management/resources/views/dashboard/index.blade.php
+++ school-management/resources/views/dashboard/index.blade.php
@@ -1,10 +1,12 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
+@section('header')
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+@endsection
+
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
@@ -201,4 +203,4 @@
             animation: fadeInUp 0.6s ease-out forwards;
         }
     </style>
-</x-app-layout>
+@endsection
