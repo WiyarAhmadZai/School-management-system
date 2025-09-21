fc6f3d9f2421512a942f1598d4ed0d68bddba093
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-21 09:13:09 +0430

    updated

--- school-management/resources/views/components/text-input.blade.php
+++ school-management/resources/views/components/text-input.blade.php
@@ -2,5 +2,5 @@
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' =>
-        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
+        'block w-full rounded-lg border-0 py-1.5 px-3 text-gray-900 dark:text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 placeholder:text-gray-400 dark:placeholder-gray-500 focus:ring-2 focus:ring-inset focus:ring-blue-600 dark:focus:ring-blue-500 focus:ring-blue-500 sm:text-sm sm:leading-6 bg-white dark:bg-gray-800 transition duration-200 ease-in-out',
 ]) !!}>
